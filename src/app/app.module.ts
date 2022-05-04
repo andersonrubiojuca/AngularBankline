3c0a284739b8fa7afdfd802fe523c4295784b7ea
Anderson Rubio Jucá Filho <andersonrubiojuca@yahoo.com.br>
2022-05-04 18:45:19 -0300

    feito os services e components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,35 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MovimentacaoListComponent } from './components/movimentacao-list/movimentacao-list.component';
+import { MovimentacaoNewComponent } from './components/movimentacao-new/movimentacao-new.component';
+import { CorrentistaComponent } from './components/correntista/correntista.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { registerLocaleData } from '@angular/common';
+import localePt from '@angular/common/locales/pt';
+import { CurrencyMaskModule } from "ng2-currency-mask";
 
+registerLocaleData(localePt);
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MovimentacaoListComponent,
+    MovimentacaoNewComponent,
+    CorrentistaComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    HttpClientModule,
+    FormsModule,
+    CurrencyMaskModule
   ],
-  providers: [],
+  providers: [{provide: LOCALE_ID, useValue: 'pt-BR'}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
